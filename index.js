d6879d1c82ac16b359281c7c006708b0e5e25f08
wkwksama <mailpakgasing@gmail.com>
2018-10-03 01:38:02 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Telegraf = require('telegraf')
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
+
+bot.startPolling()
+
+bot.catch((err) => {
+  console.log('Ooops', err)
+})
